353c3a56776abcd52ff314ee8b19097c1671fee3
szymonkrzton <75022845+szymonkrzton@users.noreply.github.com>
2022-01-13 19:25:31 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+import unittest
+import Machine as m
+import MoneyKeeper as mk
+from Ticket import Ticket
+
+class MachineTest(unittest.TestCase):
+
+    def testOverpayment(self):
+        """Kwota większa niż cena"""
+        ticketMachine = m.Machine()
+        ticket = Ticket("Ulgowy 20min", 200, "u20", "U")
+        ticketMachine.start(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+        ticketMachine.add('500', 1)
+        self.assertNotEqual(-1 or 0, ticketMachine.returnChange(ticket.returnPrice(), ticketMachine.sum()))
+
+    def testOverpaymentNoChange(self):
+        """Tylko odliczona kwota"""
+        ticketMachine = m.Machine()
+        ticket = Ticket("Ulgowy 20min", 200, "u20", "U")
+        ticketMachine.add('2000', 1)
+        self.assertEqual(-1, ticketMachine.returnChange(ticket.returnPrice(), ticketMachine.sum()))
